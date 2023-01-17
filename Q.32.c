727c746489c2db7b975b3e89e20bb125596b2afe
GAJENDRASINGH5566 <122812543+GAJENDRASINGH5566@users.noreply.github.com>
2023-01-17 17:50:01 +0530

    Add files via upload

---
+++ Q.32.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+int main()
+{
+    int source[MAX_SIZE], dest[MAX_SIZE];
+    int i, size;
+    printf("Enter the size of the array : ");
+    scanf("%d", &size);
+    printf("Enter elements of source array : ");
+    for(i=0; i<size; i++)
+    {
+        scanf("%d", &source[i]);
+    }
+    for(i=0; i<size; i++)
+    {
+        dest[i] = source[i];
+    }
+    printf("\nElements of source array are : ");
+    for(i=0; i<size; i++)
+    {
+        printf("%d\t", source[i]);
+    }
+    printf("\nElements of dest array are : ");
+    for(i=0; i<size; i++)
+    {
+        printf("%d\t", dest[i]);
+    }
+    return 0;
+}
